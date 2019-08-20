629b34cdf95f45fd7e309082e6c19b1153817863
Herminio Torres <herminiocesar@gmail.com>
2019-08-20 14:31:20 -0300

    add exercise 01 to chapter 02

---
+++ chapter-02/exercise-01.exs
@@ -0,0 +1,8 @@
+# * Create an expression that solves the following problem: Sarah has bought
+# ten slices of bread for ten cents each, three bottles of milk for two dollars
+# each, and a cake for fifteen dollars. How many dollars has Sarah spent?
+one_slice_of_bread = 0.1
+one_bottle_of_milk = 2
+one_cake = 15
+
+IO.puts (10 * one_slice_of_bread) + (3 * one_bottle_of_milk) + (1 * one_cake)
