cb025a6c12a660da41f7ace6bb558f800a9fd91a
Herminio Torres <herminiocesar@gmail.com>
2020-06-08 18:07:24 -0300

    remove inspects on 07_sort.ex

--- 04-chapter/07_sort.ex
+++ 04-chapter/07_sort.ex
@@ -1,11 +1,11 @@
 defmodule Sort do
   def ascending([]), do: []
   def ascending([x]), do: [x]
+
   def ascending(list) do
     half_size = div(Enum.count(list), 2)
     {list_a, list_b} = Enum.split(list, half_size)
-    IO.inspect("lista a: #{list_a}")
-    IO.inspect("lista b: #{list_b}")
+
     merge(
       ascending(list_a),
       ascending(list_b)
@@ -14,6 +14,7 @@ defmodule Sort do
 
   defp merge([], list_b), do: list_b
   defp merge(list_a, []), do: list_a
+
   defp merge([head_a | tail_a], list_b = [head_b | _]) when head_a <= head_b do
     [head_a | merge(tail_a, list_b)]
   end
